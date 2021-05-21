df37072ef8714501c946aa51705a92e4f56d3ea8
Saisrinivenkat <saivenkatsrini@gmail.com>
2021-05-21 16:38:28 +0530

    Added Message Feature

--- routes/post.js
+++ routes/post.js
@@ -2,19 +2,62 @@ const express = require('express');
 const { requiresAuth } = require('express-openid-connect');
 const router = express.Router()
 const Post = require('../models/model')
-const { checkAuth } = require('../config/auth_middleware')
+const Reply = require('../models/replies')
+const { checkAuth } = require('../config/auth_middleware');
+const { post } = require('./home');
+
+router.get('/' , checkAuth , (req,res) => {
+  Post.find({},(err,post) =>{
+    if(err){
+      throw err
+    }
+    
+    res.render('posts',{
+      posts: post 
+    })
+  })
+})
 
 
-router.get("/:postId", checkAuth ,function(req, res){
+
+  
+
+router.post('/ilike/:postId'  , (req,res) => {
   const requestedPostId = req.params.postId;
 
-  Post.findOne({_id: requestedPostId}, function(err, post){
+  Post.findOne({_id : requestedPostId},async (err,post) =>{
+    if(err){
+      throw err
+    }
+    
+    const presentlikes = { likes : post.likes }
+    const updatedlikes = { $set : { likes: post.likes+1 }}
+
+    await Post.updateOne(presentlikes,updatedlikes,function(err, res) {
+      if (err) throw err;
+    })
+    Post.findOne({_id:requestedPostId}, (err,post1) =>{
+      res.json({
+        like: post1.likes
+      })
+    })
+    
+  })
+})
+
+router.get("/:postId", checkAuth ,async function(req, res){
+  const requestedPostId = req.params.postId;
+  let postObj;
+  await Post.findOne({_id: requestedPostId}, function(err, post){
+    postObj = post
+  });
+  Reply.find({post_id: requestedPostId}, function(err, replies){
     res.render("post", {
-      title: post.title,
-      content: post.content
+      post:postObj,
+      comments: replies
     });
   });
-
+  
 });
 
 
